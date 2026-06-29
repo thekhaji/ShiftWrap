a9c9657490384db7ebe97eac98d35ba666b58673
thekhaji <khojiakbar_kodirov@mail.ru>
2026-06-29 14:02:40 +0900

    feat: connect mongoDB via mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,21 @@
-console.log("EXECUTED!");
+import dotenv from 'dotenv';
+dotenv.config();
+import mongoose from 'mongoose';
+import {Bot} from 'grammy'
 
+
+mongoose.connect(process.env.MONGO_URL as string)
+.then((data)=>{
+    console.log("MongoDB connected successfully");
+})
+.catch((err)=>{
+    console.error("MongoDB connection error:", err);
+});
+
+const bot = new Bot(process.env.BOT_TOKEN as string);
+
+
+bot.command('start', (ctx) => ctx.reply('Hello! I am your bot.'));
+
+bot.start();
 
